5a07472c700f5f972eeb163daada61e7bfe65a6d
quaint-racoon <92607570+quaint-racoon@users.noreply.github.com>
2025-01-01 17:56:23 +0300

    Update main.js

--- games/shooting/main.js
+++ games/shooting/main.js
@@ -319,12 +319,12 @@ class Projectile {
   }
 }
 class SinWaveProjectile extends Projectile {
-  constructor(startX, startY, radius, color, targetX, targetY, velocity, amplitude, damage = 10) {
+  constructor(startX, startY, radius, color, targetX, targetY, velocity, damage = 10) {
     super(startX, startY, radius, color, { x: 0, y: 0 }, damage); 
     this.targetX = targetX;
     this.targetY = targetY;
     this.velocity = velocity;
-    this.amplitude = amplitude;
+    this.amplitude = 5;
 
     // Calculate the initial angle based on the velocity vector
     this.angle = Math.atan2(velocity.y, velocity.x); 
